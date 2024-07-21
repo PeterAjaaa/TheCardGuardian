67f87bdaf78dc63a64d66a1851420175ef10b561
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-07-21 18:58:47 +0700

    Feature: Add Yugioh daily card channel set, unset, and time

--- thecardguardian/cogs/yugioh.py
+++ thecardguardian/cogs/yugioh.py
@@ -4,6 +4,7 @@ import datetime
 
 import aiohttp
 import discord
+from discord.commands import Option
 from discord.ext import commands, tasks
 
 
@@ -93,7 +94,7 @@ class Yugioh(commands.Cog):
                 channel = self.bot.get_channel(self.daily_card_channel_id)
                 await self.__get_and_set_random_yugioh_card()
                 await channel.send(
-                    f"Yu-Gi-Oh! Daily Card Of The Day! {datetime.datetime.now().strftime(self.DATE_FORMAT)}",  # noqa: DTZ005, E501
+                    f"Daily Yu-Gi-Oh! Card Of The Day! {datetime.datetime.now().strftime(self.DATE_FORMAT)}",  # noqa: DTZ005, E501
                     embed=self.__build_daily_embed(),
                 )
                 self.first_run = False
@@ -115,6 +116,100 @@ class Yugioh(commands.Cog):
         await ctx.respond(
             f"Daily Card Of The Day! {datetime.date.today().strftime(self.DATE_FORMAT)}",  # noqa: DTZ011, E501
             embed=self.__build_daily_embed(),
+        )
+
+    @discord.slash_command(
+        name="yugiohdailyset",
+        description="Set this channel to receive TheCardGuardian's Yu-Gi-Oh! card of the day updates",  # noqa: E501
+    )
+    async def daily_set(self, ctx: discord.ApplicationContext) -> None:
+        """Set this channel to receive TheCardGuardian's Yu-Gi-Oh! card of the day updates."""  # noqa: E501
+        if self.daily_card_channel_id == ctx.channel_id:
+            await ctx.respond(
+                f"This channel is already set to receive daily Yu-Gi-Oh! cards everyday at {self.daily_card_hour}:{self.daily_card_minute}.",  # noqa: E501
+            )
+            return
+
+        if self.daily_card_channel_id is not None:
+            await ctx.respond(
+                "Another channel is already set to receive Yu-Gi-Oh! daily cards. Please unset that first.",  # noqa: E501
+            )
+
+        self.daily_card_channel_id = ctx.channel_id
+        await ctx.respond(
+            "Yu-Gi-Oh! daily card set to this channel! Type `/yugiohdailytime` to set the time for the Yu-Gi-Oh! daily card to be sent.",  # noqa: E501
+        )
+
+    @discord.slash_command(
+        name="yugiohdailyunset",
+        description="Unset this channel to receive TheCardGuardian's Yu-Gi-Oh! card of the day updates",  # noqa: E501
+    )
+    async def daily_unset(self, ctx: discord.ApplicationContext) -> None:
+        """Unset this channel as the receiver for TheCardGuardian's Yu-Gi-Oh! daily card of the day updates."""  # noqa: E501
+        if (
+            self.daily_card_channel_id != ctx.channel_id
+            or self.daily_card_channel_id is None
+        ):
+            await ctx.respond(
+                "This channel is not set to receive Yu-Gi-Oh! daily cards.",
+            )
+            return
+
+        self.daily_card_channel_id = None
+        await ctx.respond(
+            "Daily card unset! Type `/yugiohdailyset` to set this channel to receive the daily Yu-Gi-Oh! card of the day.",  # noqa: E501
+        )
+
+    @discord.slash_command(
+        name="yugiohdailytime",
+        description="Set the time at which the daily Yu-Gi-Oh! card should be sent, in 24 hour format (ex: 17:00)",  # noqa: E501
+    )
+    async def daily_time(
+        self,
+        ctx: discord.ApplicationContext,
+        time: str = Option(str, "24 hour format (ex: 17:00), use 00:00 for midnight"),
+    ) -> None:
+        """Set the time at which the daily Yu-Gi-Oh! card should be sent, in 24 hour format (ex: 17:00)."""  # noqa: E501
+        if self.daily_card_channel_id is None:
+            await ctx.respond(
+                "No channel is set to receive daily Yu-Gi-Oh! cards. Please set one first using `/yugiohdailyset`.",  # noqa: E501
+            )
+            return
+
+        time_split = time.split(":")
+
+        if len(time_split) != self.PROPER_SPLITTED_TIME_LENGTH:
+            await ctx.respond(
+                "Invalid time format. Please use 24 hour format (ex: 17:00)",
+            )
+            return
+
+        if int(time_split[0]) > self.MAX_HOUR or int(time_split[1]) > self.MAX_SECOND:
+            await ctx.respond(
+                "Invalid time format. Please use 24 hour format (ex: 17:00)",
+            )
+            return
+
+        if int(time_split[0]) < 0 or int(time_split[1]) < 0:
+            await ctx.respond(
+                "Invalid time format. Please use 24 hour format (ex: 17:00)",
+            )
+            return
+
+        try:
+            self.daily_card_hour = int(time_split[0])
+            self.daily_card_minute = int(time_split[1])
+        except ValueError:
+            await ctx.respond(
+                "Invalid time format. Please use 24 hour format (ex: 17:00)",
+            )
+            return
+
+        await ctx.respond(
+            "Daily Yu-Gi-Oh! card time set to "
+            + str(time_split[0])
+            + ":"
+            + str(time_split[1]),
         )
 
 
